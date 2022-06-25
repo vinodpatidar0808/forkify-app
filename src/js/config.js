f332598006631bc5ff4f49c8eab8de8453b419e1
vinod_0808 <vinodpatidar0808@gmail.com>
2022-06-25 14:45:07 +0530

    helpers and configuration files

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+// config file generally contains constant variables which can be used across entire application
+// uppercase : to show that this is a constant that never changes
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,15 @@
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
+
 export const state = {
     recipe: {},
 };
 
 export const loadRecipe = async function (id) {
     try {
-        const res = await fetch(
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-        );
-        const data = await res.json();
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-        // console.log(res, data);
-        //format data
+        const data = await getJSON(`${API_URL}/${id}`);
+        
+
         let { recipe } = data.data;
         state.recipe = {
             id: recipe.id,
@@ -25,6 +24,7 @@ export const loadRecipe = async function (id) {
 
         // console.log(state.recipe);
     } catch (err) {
-        console.error(err);
+        console.error(`${err} 💥💥💥💥`);
     }
 };
+
