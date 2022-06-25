2f8002775a4d58ff128cd595dc66c7556dff1fe7
vinod_0808 <vinodpatidar0808@gmail.com>
2022-06-25 12:12:08 +0530

    refactor for MVC architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,8 @@
 //NOTE: api link:--> https://forkify-api.herokuapp.com/v2
 
-import icons from 'url:../img/icons.svg';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 // polyfilling packages
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -17,153 +19,32 @@ const timeout = function (s) {
 };
 
 ///////////////////////////////////////
-const renderSpinner = function (parentEl) {
-    const markup = `      <div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div>`;
-    parentEl.innerHTML = '';
-    parentEl.insertAdjacentHTML('afterbegin', markup);
-};
 
 //---TASK---: Loading recipe
-const showRecipe = async function () {
+const controlRecipes = async function () {
     const id = window.location.hash.slice(1);
     if (!id) return;
     try {
-        renderSpinner(recipeContainer);
-        const res = await fetch(
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-        );
-        const data = await res.json();
-        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-        // console.log(res, data);
-        //format data
-        let { recipe } = data.data;
-        recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            publisher: recipe.publisher,
-            sourceUrl: recipe.source_url,
-            image: recipe.image_url,
-            servings: recipe.servings,
-            cookingTime: recipe.cooking_time,
-            ingredients: recipe.ingredients,
-        };
-
-        console.log(recipe.ingredients);
-        const markup = `
-            <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${
-            recipe.title
-        }" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
-
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-                recipe.cookingTime
-            }</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-                recipe.servings
-            }</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipe.ingredients
-              .map(ingredient => {
-                  return `
-                <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                    <use href="${icons}#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">${ingredient.quantity}</div>
-                <div class="recipe__description">
-                    <span class="recipe__unit">${ingredient.unit}</span>
-                    ${ingredient.description}
-                </div>
-                </li>`;
-              })
-              .join('')}
-
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-                recipe.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>`;
-        recipeContainer.innerHTML = '';
-        recipeContainer.insertAdjacentHTML('afterbegin', markup);
+        recipeView.renderSpinner();
+        // NOTE: loadRecipe is an async function
+        await model.loadRecipe(id);
+        // const { recipe } = model.state;
+        // console.log('model.state.recipe ', model.state.recipe);
+        recipeView.render(model.state.recipe);
     } catch (err) {
-        alert(err);
+        console.error(err);
     }
 };
 
-showRecipe();
+// controlRecipes();
 
 // ---TASK---:
 // hashchange is an event for detecting changes in url #part
 
 // we need load event because when we copy url and run it in another tab it doesn't work because hash is not changed
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
-// what if we had to run showRecipe fxn for many events: do like this
-['hash', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
+// what if we had to run controlRecipes fxn for many events: do like this
+['hashchange', 'load'].forEach(ev =>
+    window.addEventListener(ev, controlRecipes)
+);
